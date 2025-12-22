8f522782d930f10e2d1f6c8f3d0e9e7f4e77d80a
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-12-22 23:50:36 +0530

    about page is being constructed

--- personal-portfolio/src/pages/HOME.jsx
+++ personal-portfolio/src/pages/HOME.jsx
@@ -21,6 +21,10 @@ const HOME = () => {
       <div className={home.herodev}><p className={home.front}>frontend developer</p><div className={home.image}><img alt="image1" src={heroVector}/></div></div>
     </section>
     <section className={home.aboutsec}>
+      <div className={home.mainabout}>
+        <div className={home.aboutpagetitle}><div className={home.aboutme}><p>about me</p><p id={home.uniqueaboutme}>about me</p><p>about me</p></div><div className={home.aboutme} aria-hidden="true"><p>about me</p><p id={home.uniqueaboutme}>about me</p><p>about me</p></div></div><div className={home.aboutline}></div>
+        <div className={home.aboutpage}></div>
+      </div>
     </section>
     </>
   )
