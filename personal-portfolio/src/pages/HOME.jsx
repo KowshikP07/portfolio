eb18e41979307570c3cad46227f3421cb1c1dcc3
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-12-26 23:23:29 +0530

    designing the about page

--- personal-portfolio/src/pages/HOME.jsx
+++ personal-portfolio/src/pages/HOME.jsx
@@ -42,25 +42,26 @@ const HOME = () => {
           <div className={home.aboutpage}>
             <div className={home.aboutpagehero}>
               <div className={home.aboutmebav}>
-              <span className={home.navbarlogo}>ABOUT me</span>
-              <ul className={home.aboutmenavlink}>
-              <li className={home.aboutnavli}>who i am</li>
-              <li className={home.aboutnavli}>who i was</li>
-              <li className={home.aboutnavli}>faq</li></ul>
-            </div>
-            <section className={home.sectionhome}>
-            <div className={home.aboutmename}><h1 className={home.aboutmenameyear}>KOWSHIK'05</h1></div>
-            <div className={home.sectionhomeabout}>
-              <div className={home.aboutmephoto}></div>
-            <div className={home.aboutmecontent}></div>
-            </div>
-            </section>
-            <section className={home.aboutwhoiam}>
-              <div className={home.aboutwhoiaminfinitescroll}><h1 className={home.aboutmenameyearbottom}>KOWSHIK'05</h1></div>
-              <div className={home.aboutwhoiamcontent}>
-                <div className={home.aboutwhoiamwords}></div>
+                <span className={home.navbarlogo}>ABOUT me</span>
+                <ul className={home.aboutmenavlink}>
+                  <li className={home.aboutnavli}>who i am</li>
+                  <li className={home.aboutnavli}>who i was</li>
+                  <li className={home.aboutnavli}>faq</li></ul>
               </div>
-            </section>
+              <section className={home.sectionhome}>
+                <div className={home.aboutmename}><h1 className={home.aboutmenameyear}>KOWSHIK'05</h1></div>
+                <div className={home.sectionhomeabout}>
+                  <div className={home.aboutmephoto}></div>
+                  <div className={home.aboutmecontent}></div>
+                </div>
+              </section>
+              <section className={home.aboutwhoiam}>
+                <div className={home.aboutwhoiaminfinitescroll}><div className={home.scrolling}><div className={home.aboutmetriangle}></div><h1 className={home.aboutmenameyearbottom}>KOWSHIK'05</h1><div className={home.aboutmetriangle}></div><h1 className={home.aboutmenameyearbottom}>KOWSHIK'05</h1><div className={home.aboutmetriangle}></div></div>
+                <div className={home.scrolling} aria-hidden="true"><div className={home.aboutmetriangle}></div><h1 className={home.aboutmenameyearbottom}>KOWSHIK'05</h1><div className={home.aboutmetriangle}></div><h1 className={home.aboutmenameyearbottom}>KOWSHIK'05</h1><div className={home.aboutmetriangle}></div></div></div>
+                <div className={home.aboutwhoiamcontent}>
+                  <div className={home.aboutwhoiamwords}></div>
+                </div>
+              </section>
             </div>
           </div>
         </div>
