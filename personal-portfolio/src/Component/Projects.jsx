886177d11ec79f1cd21e504931f61993ac629f94
KowshikP07 <kowshikp.23cse@kongu.edu>
2026-01-04 01:15:41 +0530

    almost done project page

--- personal-portfolio/src/Component/Projects.jsx
+++ personal-portfolio/src/Component/Projects.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import project from '../Style/Projects.module.css'
+import qr from '../assets/qr.png'
+import dark from '../assets/dark.png'
+import skill from '../assets/skillcave.png'
 const Projects = () => {
-    const [projectdone,setprojectdone]=useState([{"id":1,"title":"Notes Application"},{"id":2,"title":"Notes Application"},{"id":3,"title":"Notes Application"},{"id":4,"title":"Notes Application"}]);
+    const [projectdone,setprojectdone]=useState([{"id":1,"title":"Notes Application","image":dark},{"id":2,"title":"Notes Application","image":skill},{"id":3,"title":"Notes Application","image":dark},{"id":4,"title":"Notes Application","image":skill}]);
+    const projectcompleted=projectdone.map((projects,index)=>(
+      <div className={project.projectdisplay} key={index}><div className={project.projectimage}><img src={projects.image} alt={projects.title}></img></div><div className={project.projectdesc}>{projects.title} <img src={qr} alt="qr"></img></div></div>
+    ))
   return (
     <div className={project.main}>
         <h1 className={project.title}>PROJECTS</h1>
         <div className={project.toshow}>
-            <div className={project.projectdisplay}><div className={project.projectimage}></div><div className={project.projectdesc}></div></div>
-            <div className={project.projectdisplay}><div className={project.projectimage}></div><div className={project.projectdesc}></div></div>
-            <div className={project.projectdisplay}><div className={project.projectimage}></div><div className={project.projectdesc}></div></div>
-            <div className={project.projectdisplay}><div className={project.projectimage}></div><div className={project.projectdesc}></div></div>
+            {projectcompleted}
         </div>
     </div>
   )
