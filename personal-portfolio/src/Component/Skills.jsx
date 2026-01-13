97887eb98b6674025a6396d3b54ddf2ff2875037
KowshikP07 <kowshikp.23cse@kongu.edu>
2026-01-13 22:18:36 +0530

    created a separate component for showcasing achievements

--- personal-portfolio/src/Component/Skills.jsx
+++ personal-portfolio/src/Component/Skills.jsx
@@ -3,18 +3,29 @@ import skill from '../Style/Skill.module.css'
 const Skills = () => {
   return (
     <>
-    <div className={skill.hero}>
+      <div className={skill.hero}>
         <div className={skill.title}><h1>Skills</h1></div>
         <div className={skill.container}>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div>
-        <div className={skill.skillcontainer}></div></div>
-    </div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div>
+          <div className={skill.skillcontainer}></div></div>
+      </div>
+      <div className={skill.certificate}>
+        <div className={skill.title}>
+          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+          </div>
+        <div className={skill.carousel}></div>
+      </div>
     </>
   )
 }
