3766a17e0d1c7d3319143f447edcd7b88813e160
KowshikP07 <kowshikp.23cse@kongu.edu>
2026-01-14 13:42:32 +0530

    added the video for achievement section

--- personal-portfolio/src/Component/Skills.jsx
+++ personal-portfolio/src/Component/Skills.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import skill from '../Style/Skill.module.css'
+import achieve from '../assets/achievements.mp4'
 const Skills = () => {
   return (
-    <>
+    <div className={skill.main}>
       <div className={skill.hero}>
         <div className={skill.title}><h1>Skills</h1></div>
         <div className={skill.container}>
@@ -16,17 +17,20 @@ const Skills = () => {
           <div className={skill.skillcontainer}></div></div>
       </div>
       <div className={skill.certificate}>
-        <div className={skill.title}>
-          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
-          <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
+        <video autoPlay  muted  loop  playsInline className={skill.vid}><source src={achieve} type="video/mp4"/></video>
+        <div className={skill.titlecert}>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           </div>
-        <div className={skill.carousel}></div>
+        <div className={skill.carousel}>
+          <button></button>
+          <div className={skill.carouselcont}></div>
+          <button></button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
