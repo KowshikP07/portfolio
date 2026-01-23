af8abbcffc0adb552b7762f51b548fa1ff0a59a8
KowshikP07 <kowshikp.23cse@kongu.edu>
2026-01-23 23:52:56 +0530

    carousel added need to add image

--- personal-portfolio/src/Component/Skills.jsx
+++ personal-portfolio/src/Component/Skills.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import skill from '../Style/Skill.module.css'
 import achieve from '../assets/achievements.mp4'
 const Skills = () => {
+  const [index, setIndex] = useState(0);
+  const rightclick = () => {
+    setIndex(prev => (prev + 1)%3);
+    console.log(`right : ${index}`);
+  }
+  const leftclick = () => {
+
+    setIndex(prev => (prev + 1)%3);
+    console.log(`left : ${index}`);
+  }
   return (
     <div className={skill.main}>
       <div className={skill.hero}>
@@ -17,17 +27,25 @@ const Skills = () => {
           <div className={skill.skillcontainer}></div></div>
       </div>
       <div className={skill.certificate}>
-        <video autoPlay  muted  loop  playsInline className={skill.vid}><source src={achieve} type="video/mp4"/></video>
+        <video autoPlay muted loop playsInline className={skill.vid}><source src={achieve} type="video/mp4" /></video>
         <div className={skill.titlecert}>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
           <div className={skill.certiTitle}><p>ACHIEVEMENTS</p><p>ACHIEVEMENTS</p></div>
-          </div>
+        </div>
         <div className={skill.carousel}>
-          <button></button>
-          <div className={skill.carouselcont}></div>
-          <button></button>
+          <button onClick={rightclick}></button>
+          <div className={skill.carouselcont}>
+            <div className={skill.track} style={{
+              transform: `translateX(-${index * 100}%)`
+            }}>
+              <div className={skill.slide}></div>
+              <div className={skill.slide}></div>
+              <div className={skill.slide}></div>
+            </div>
+          </div>
+          <button onClick={leftclick}></button>
         </div>
       </div>
     </div>
