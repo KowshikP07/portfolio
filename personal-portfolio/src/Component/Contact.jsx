094dbe6abf6d49bba46d9f2d4646d4446f66509b
KowshikP07 <kowshikp.23cse@kongu.edu>
2026-01-27 22:27:17 +0530

    link has benn added and updated in icons

--- personal-portfolio/src/Component/Contact.jsx
+++ personal-portfolio/src/Component/Contact.jsx
@@ -3,6 +3,9 @@ import con from '../Style/contact.module.css'
 import logo from '../assets/logo.png'
 import selfpor from '../assets/selfportrait1.jpg'
 import selfpor3 from '../assets/selfportrait3.png'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 const Contact = () => {
   return (
     <>
@@ -37,6 +40,22 @@ const Contact = () => {
           <div className={con.links}>
             <div className={con.mainlink}>
             <span className={con.profilelink}>
+                <span><a
+        href="https://github.com/KowshikP07"
+        target="_blank"
+      rel="noopener noreferrer"
+    ><FaGithub className={con.icons}/>
+    </a></span>
+              <span><a
+        href="https://www.linkedin.com/in/kowshikprakasam"
+        target="_blank"
+      rel="noopener noreferrer"
+    ><FaLinkedin className={con.icons}/></a></span>
+              <span><a
+        href="https://leetcode.com/u/KOWSHIK_P/"
+        target="_blank"
+      rel="noopener noreferrer"
+    ><SiLeetcode className={con.icons}/></a></span>
             </span>
             <span className={con.madeby}>Made with Love by Kowshik</span>
             </div>
