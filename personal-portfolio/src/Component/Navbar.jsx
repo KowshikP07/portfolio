503e7c486c4cc8a74a9e577c55e7b74abdee7105
KowshikP07 <kowshikp.23cse@kongu.edu>
2026-04-01 21:48:08 +0530

    added hamburger symbol and make navbar responsive to the various screen size

--- personal-portfolio/src/Component/Navbar.jsx
+++ personal-portfolio/src/Component/Navbar.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import nav from '../Style/Nav.module.css'
 import logo from '../assets/logo.png'
 import { Link } from "react-scroll";
+
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => setMenuOpen(prev => !prev);
+    const closeMenu = () => setMenuOpen(false);
+
     return (
         <>
-            <nav className={nav}>
+            <nav className={nav.navbar}>
                 <div className={nav.bar}>
                     <div className={nav.logo}><img src={logo} alt="logo" /></div>
-                    <div className={nav.navlink}>
+
+                    {/* Hamburger Button */}
+                    <button
+                        className={nav.hamburger}
+                        onClick={toggleMenu}
+                        aria-label="Toggle navigation menu"
+                        aria-expanded={menuOpen}
+                    >
+                        <span className={`${nav.line} ${menuOpen ? nav.lineTop : ''}`}></span>
+                        <span className={`${nav.line} ${menuOpen ? nav.lineMiddle : ''}`}></span>
+                        <span className={`${nav.line} ${menuOpen ? nav.lineBottom : ''}`}></span>
+                    </button>
+
+                    {/* Nav Links */}
+                    <div className={`${nav.navlink} ${menuOpen ? nav.navlinkOpen : ''}`}>
                         <ul>
                             <li className={nav.links}>
-                                <Link to="home" smooth={true} duration={500}>Home</Link>
+                                <Link to="home" smooth={true} duration={500} onClick={closeMenu}>Home</Link>
                             </li>
-
                             <li className={nav.links}>
-                                <Link to="about" smooth={true} duration={500}>About</Link>
+                                <Link to="about" smooth={true} duration={500} onClick={closeMenu}>About</Link>
                             </li>
-
                             <li className={nav.links}>
-                                <Link to="projects" smooth={true} duration={500}>Projects</Link>
+                                <Link to="projects" smooth={true} duration={500} onClick={closeMenu}>Projects</Link>
                             </li>
-
                             <li className={nav.links}>
-                                <Link to="skills" smooth={true} duration={500}>Skills</Link>
+                                <Link to="skills" smooth={true} duration={500} onClick={closeMenu}>Skills</Link>
                             </li>
-
                             <li className={nav.links}>
-                                <Link to="contact" smooth={true} duration={500}>Contact</Link>
+                                <Link to="contact" smooth={true} duration={500} onClick={closeMenu}>Contact</Link>
                             </li>
                         </ul>
                     </div>
